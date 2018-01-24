e6b2abdb8215c4f5f8d90014b12d3810702b470c
nylaz <lasseman@gmail.com>
2018-01-24 16:12:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,12 @@
 
 jversion=1.8.0
 
+#Installing java
 yum install java-${jversion}-openjdk-devel
+
+#Installing docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce
