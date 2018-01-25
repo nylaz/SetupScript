803075e38050f8db2ee860ed11bdaf4ac355f682
nylaz <lasseman@gmail.com>
2018-01-25 13:09:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,20 @@
 
 jversion=1.8.0
 
-#Installing java
+#install wget
+yum install wget
+
+#installing java
 yum install java-${jversion}-openjdk-devel
 
-#Installing docker
+#installing docker
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 yum install docker-ce
+
+#Installing jenkins
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins
